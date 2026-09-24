e966ecd03ef18c59949903ea937283f58d102387
Slayleen <28saytekin@castilleja.org>
2026-09-24 16:34:16 -0700

    removed slowdrive and reset heading buttons for workshop

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -326,15 +326,6 @@ public class RobotContainer {
                                   hoodSubsystem,
                                   intakeSubsystem))));
 
-      // Back -- Slow Drive Toggle
-      /*controller
-      .back()
-      .onTrue(
-          Commands.runOnce(
-              () -> {
-                drive.toggleSlowDrive();
-              },
-              drive));*/
 
       // Left Trigger -- Shoot with Turret (while true)
       /*controller
@@ -607,27 +598,7 @@ public class RobotContainer {
                     }
                   }));
 
-      // B -- Reset Heading
-      controller
-          .b()
-          .onTrue(
-              Commands.runOnce(
-                      () -> {
-                        if (DriverStation.getAlliance().isPresent()
-                            && DriverStation.getAlliance().get() == DriverStation.Alliance.Red) {
-                          drive.setPose(
-                              new Pose2d(
-                                  drive.getPose().getTranslation(),
-                                  new Rotation2d(Math.toRadians(180))));
-                        } else {
-                          drive.setPose(
-                              new Pose2d(
-                                  drive.getPose().getTranslation(),
-                                  new Rotation2d(Math.toRadians(0))));
-                        }
-                      },
-                      drive)
-                  .ignoringDisable(true));
+ 
 
       controller
           .x()
@@ -643,15 +614,6 @@ public class RobotContainer {
                                   hoodSubsystem,
                                   intakeSubsystem))));
 
-      // Y -- Slow Drive Toggle
-      controller
-          .y()
-          .onTrue(
-              Commands.runOnce(
-                  () -> {
-                    drive.toggleSlowDrive();
-                  },
-                  drive));
 
       // Left Trigger -- Shoot with Turret (while true)
       controller
